c39e501cc464d3bd0238ec60cc4c2408a3010b45
safi-0412 <omonjanovas@gmail.com>
2025-01-31 09:59:43 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,348 @@
+function btn(){
+    const narhi = 45.000;
+    const chislo = +prompt("Qiyirik setidn nechta kerk?");
+
+    switch (chislo > 0) {
+        case true:
+            const obshiy = chislo * narhi;
+            const potverjdeniye = confirm(`
+                ${chislo} Qiyirik jami narxi ${obshiy} so'm
+
+                Mahsulotni zacaz qilasizmi?`)
+
+            switch (potverjdeniye){
+                case true:
+                    alert("Zakaz qabul qilindi")
+                    break;
+            
+                default:
+                    alert("Zakaz qabul qilinmadi")
+                    break;
+            }
+            break;
+    
+        default:
+            alert("Vvedite pravilnoe chislo");
+            break;
+    }
+}
+
+
+function btnn(){
+    const narhii = 79.000;
+    const chisloo = +prompt("Снек сет nechta kerk?");
+
+    switch (chisloo > 0) {
+        case true:
+            const obshiyy = chisloo * narhii;
+            const potverjdeniyee = confirm(`
+                ${chisloo} Снек сет narxi ${obshiyy} so'm
+
+                Mahsulotni zacaz qilasizmi?`)
+
+            switch (potverjdeniyee){
+                case true:
+                    alert("Zakaz qabul qilindi")
+                    break;
+            
+                default:
+                    alert("Zakaz qabul qilinmadi")
+                    break;
+            }
+            break;
+    
+        default:
+            alert("Vvedite pravilnoe chislo");
+            break;
+    }
+}
+
+function btnnn(){
+    const narhiii = 45.000;
+    const chislooo = +prompt("Лонгер nechta kerk?");
+
+    switch (chislooo > 0) {
+        case true:
+            const obshiyyy = chislooo * narhiii;
+            const potverjdeniyeee = confirm(`
+                ${chislooo} Лонгер narxi ${obshiyyy} so'm
+
+                Mahsulotni zacaz qilasizmi?`)
+
+            switch (potverjdeniyeee){
+                case true:
+                    alert("Zakaz qabul qilindi")
+                    break;
+            
+                default:
+                    alert("Zakaz qabul qilinmadi")
+                    break;
+            }
+            break;
+    
+        default:
+            alert("Vvedite pravilnoe chislo");
+            break;
+    }
+}
+
+function btnnnn (){
+    const narhiii = 34.000;
+    const chislooo = +prompt("Классик сет nechta kerk?");
+
+    switch (chislooo > 0) {
+        case true:
+            const obshiyyy = chislooo * narhiii;
+            const potverjdeniyeee = confirm(`
+                ${chislooo} Классик сет narxi ${obshiyyy} so'm
+
+                Mahsulotni zacaz qilasizmi?`)
+
+            switch (potverjdeniyeee){
+                case true:
+                    alert("Zakaz qabul qilindi")
+                    break;
+            
+                default:
+                    alert("Zakaz qabul qilinmadi")
+                    break;
+            }
+            break;
+    
+        default:
+            alert("Vvedite pravilnoe chislo");
+            break;
+    }
+}
+
+function btnnnnn (){
+    const narhiii = 62.000;
+    const chislooo = +prompt("Биг сет nechta kerk?");
+
+    switch (chislooo > 0) {
+        case true:
+            const obshiyyy = chislooo * narhiii;
+            const potverjdeniyeee = confirm(`
+                ${chislooo} Биг сет narxi ${obshiyyy} so'm
+
+                Mahsulotni zacaz qilasizmi?`)
+
+            switch (potverjdeniyeee){
+                case true:
+                    alert("Zakaz qabul qilindi")
+                    break;
+            
+                default:
+                    alert("Zakaz qabul qilinmadi")
+                    break;
+            }
+            break;
+    
+        default:
+            alert("Vvedite pravilnoe chislo");
+            break;
+    }
+}
+
+function bbtn (){
+    const narhiii = 20.000;
+    const chislooo = +prompt("Комбо сет Большой nechta kerk?");
+
+    switch (chislooo > 0) {
+        case true:
+            const obshiyyy = chislooo * narhiii;
+            const potverjdeniyeee = confirm(`
+                ${chislooo} Комбо сет Большой narxi ${obshiyyy} so'm
+
+                Mahsulotni zacaz qilasizmi?`)
+
+            switch (potverjdeniyeee){
+                case true:
+                    alert("Zakaz qabul qilindi")
+                    break;
+            
+                default:
+                    alert("Zakaz qabul qilinmadi")
+                    break;
+            }
+            break;
+    
+        default:
+            alert("Vvedite pravilnoe chislo");
+            break;
+    }
+}
+
+function bbbtn (){
+    const narhiii = 53.000;
+    const chislooo = +prompt("Лестер сетnechta kerk?");
+
+    switch (chislooo > 0) {
+        case true:
+            const obshiyyy = chislooo * narhiii;
+            const potverjdeniyeee = confirm(`
+                ${chislooo} Лестер сетjami narxi ${obshiyyy} so'm
+
+                Mahsulotni zacaz qilasizmi?`)
+
+            switch (potverjdeniyeee){
+                case true:
+                    alert("Zakaz qabul qilindi")
+                    break;
+            
+                default:
+                    alert("Zakaz qabul qilinmadi")
+                    break;
+            }
+            break;
+    
+        default:
+            alert("Vvedite pravilnoe chislo");
+            break;
+    }
+}
+
+function bttn (){
+    const narhiii = 36.000;
+    const chislooo = +prompt("Скул сет nechta kerk?");
+
+    switch (chislooo > 0) {
+        case true:
+            const obshiyyy = chislooo * narhiii;
+            const potverjdeniyeee = confirm(`
+                ${chislooo} Скул сет narxi ${obshiyyy} so'm
+
+                Mahsulotni zacaz qilasizmi?`)
+
+            switch (potverjdeniyeee){
+                case true:
+                    alert("Zakaz qabul qilindi")
+                    break;
+            
+                default:
+                    alert("Zakaz qabul qilinmadi")
+                    break;
+            }
+            break;
+    
+        default:
+            alert("Vvedite pravilnoe chislo");
+            break;
+    }
+}
+
+
+function bttn (){
+    const narhiii = 43.000;
+    const chislooo = +prompt("Do’stlar 1х nechta kerk?");
+    switch (chislooo > 0) {
+        case true:
+            const obshiyyy = chislooo * narhiii;
+            const potverjdeniyeee = confirm(`
+                ${chislooo} Do’stlar 1х narxi ${obshiyyy} so'm
+
+                Mahsulotni zacaz qilasizmi?`)
+
+            switch (potverjdeniyeee){
+                case true:
+                    alert("Zakaz qabul qilindi")
+                    break;
+            
+                default:
+                    alert("Zakaz qabul qilinmadi")
+                    break;
+            }
+            break;
+    
+        default:
+            alert("Vvedite pravilnoe chislo");
+            break;
+    }
+}
+
+function bttttn (){
+    const narhiii = 79.000;
+    const chislooo = +prompt("Do’stlar 2х nechta kerk?");
+
+    switch (chislooo > 0) {
+        case true:
+            const obshiyyy = chislooo * narhiii;
+            const potverjdeniyeee = confirm(`
+                ${chislooo} Do’stlar 2х ${obshiyyy} so'm
+
+                Mahsulotni zacaz qilasizmi?`)
+
+            switch (potverjdeniyeee){
+                case true:
+                    alert("Zakaz qabul qilindi")
+                    break;
+            
+                default:
+                    alert("Zakaz qabul qilinmadi")
+                    break;
+            }
+            break;
+    
+        default:
+            alert("Vvedite pravilnoe chislo");
+            break;
+    }
+}
+
+function non(){
+    const narhiii = 149.000;
+    const chislooo = +prompt("Do’stlar 4х nechta kerk?");
+
+    switch (chislooo > 0) {
+        case true:
+            const obshiyyy = chislooo * narhiii;
+            const potverjdeniyeee = confirm(`
+                ${chislooo} Do’stlar 4х narxi ${obshiyyy} so'm
+
+                Mahsulotni zacaz qilasizmi?`)
+
+            switch (potverjdeniyeee){
+                case true:
+                    alert("Zakaz qabul qilindi")
+                    break;
+            
+                default:
+                    alert("Zakaz qabul qilinmadi")
+                    break;
+            }
+            break;
+    
+        default:
+            alert("Vvedite pravilnoe chislo");
+            break;
+    }
+}
+
+function none (){
+    const narhiii = 43.000;
+    const chislooo = +prompt("Острый Do’stlar 1х nechta kerk?");
+
+    switch (chislooo > 0) {
+        case true:
+            const obshiyyy = chislooo * narhiii;
+            const potverjdeniyeee = confirm(`
+                ${chislooo} Острый Do’stlar 1х narxi ${obshiyyy} so'm
+
+                Mahsulotni zacaz qilasizmi?`)
+
+            switch (potverjdeniyeee){
+                case true:
+                    alert("Zakaz qabul qilindi")
+                    break;
+            
+                default:
+                    alert("Zakaz qabul qilinmadi")
+                    break;
+            }
+            break;
+    
+        default:
+            alert("Vvedite pravilnoe chislo");
+            break;
+    }
+}
